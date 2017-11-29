f35dddbf0abe5b3364dc8f587a57565058740a71
Pavan Kumar <pavan71198@users.noreply.github.com>
2017-11-29 17:00:23 +0530

    Linking all pages and Added components

--- faculty/urls.py
+++ faculty/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^signup$',views.signup, name='signup'),
-    url(r'^department/(?P<name>[a-zA-Z]+)$',views.department, name='department'),
+    url(r'^department/(?P<name>[a-zA-Z]+)$',views.departmentview, name='department'),
     # url(r'^search$',views.search, name='search'),
     url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)$', views.profile, name='profile'),
     url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/edit$', views.profileedit, name='profileedit'),
@@ -17,10 +17,13 @@ urlpatterns = [
     url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/projects$', views.projectsview, name='projects'),
     url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/projects/add$', views.projectadd, name='projectadd'),
     url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/projects/delete$', views.projectdel, name='projectdelete'),
-    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/publications$', views.publicationsview, name='publications'),
-    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/publications/add$', views.publicationadd, name='publicationadd'),
-    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/publications/delete$', views.publicationdel, name='publicationdelete'),
-    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/recognitions$', views.recognitionsview, name='recognitions'),
-    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/recognitions/add$', views.recognitionadd, name='recognitionadd'),
-    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/recognitions/delete$', views.recognitiondel, name='recognitiondelete')
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/publications$', views.publicationsview, name='publications'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/publications/add$', views.publicationadd, name='publicationadd'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/publications/delete$', views.publicationdel, name='publicationdelete'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/recognitions$', views.recognitionsview, name='recognitions'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/recognitions/add$', views.recognitionadd, name='recognitionadd'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/recognitions/delete$', views.recognitiondel, name='recognitiondelete'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/experiences$', views.experiencesview, name='experiences'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/experiences/add$', views.experienceadd, name='experienceadd'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/experiences/delete$', views.experiencedel, name='experiencedelete')
 ]
