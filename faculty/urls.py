c80e9489b794c8831a58f3dae77c73b4afed7606
Pavan Kumar <pavan71198@users.noreply.github.com>
2017-11-29 12:03:36 +0530

    Added projects pages

--- faculty/urls.py
+++ faculty/urls.py
@@ -3,6 +3,24 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^signup$',views.signup, name='signup')
+    url(r'^signup$',views.signup, name='signup'),
+    url(r'^department/(?P<name>[a-zA-Z]+)$',views.department, name='department'),
+    # url(r'^search$',views.search, name='search'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)$', views.profile, name='profile'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/edit$', views.profileedit, name='profileedit'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/students$', views.studentsview, name='students'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/students/add$', views.studentadd, name='studentadd'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/students/delete$',views.studentdel, name='studentdelete'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/teachings$', views.teachingview, name='teachings'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/teaching/add$', views.teachingadd, name='teachingadd'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/teaching/delete$', views.teachingdel, name='teachingdelete'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/projects$', views.projectsview, name='projects'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/projects/add$', views.projectadd, name='projectadd'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/projects/delete$', views.projectdel, name='projectdelete'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/publications$', views.publicationsview, name='publications'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/publications/add$', views.publicationadd, name='publicationadd'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/publications/delete$', views.publicationdel, name='publicationdelete'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/recognitions$', views.recognitionsview, name='recognitions'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/recognitions/add$', views.recognitionadd, name='recognitionadd'),
+    # url(r'^profile/(?P<username>[a-zA-Z0-9_.]+)/recognitions/delete$', views.recognitiondel, name='recognitiondelete')
 ]
